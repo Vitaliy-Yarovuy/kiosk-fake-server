b8cd804aab9a1382c11f7b450b935b55abd3ba2f
Vitaliy <vitaliy.yxz@gmail.com>
2014-02-24 19:43:01 +0200

    - update

--- public/client/kiosk.js
+++ public/client/kiosk.js
@@ -29,8 +29,8 @@
             size = wndSize();
         }
 
-        if((point.x == "left" && x < 0.1 * size.width) || (point.x == "right" && x > 0.9 * size.width)){
-            if((point.y == "top" && y < 0.1 * size.height) || (point.y == "bottom" && y > 0.9 * size.height)){
+        if((point.x == "left" && x < 0.2 * size.width) || (point.x == "right" && x > 0.8 * size.width)){
+            if((point.y == "top" && y < 0.2 * size.height) || (point.y == "bottom" && y > 0.8 * size.height)){
                 if(points.length <= ++openStep){
                     kioskWrapperClient.openSettings();
                 }
